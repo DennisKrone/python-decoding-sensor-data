df513bb9f55c2352826eaf4c4f9c745503cd93f7
Dennis Kroné <dennis.krone@svenskaspel.se>
2020-09-15 14:37:04 +0200

    implement module2

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -1,4 +1,5 @@
 # Runner script for all modules
+from house_info import HouseInfo
 from load_data import load_sensor_data
 
 ##############################
@@ -12,7 +13,10 @@ print("Sensor Data App")
 data = load_sensor_data()
 print(f"Loaded records: {len(data)}")
 # Module 2 code here:
-
+house_info = HouseInfo(data)
+test_area = 1
+recs = house_info.get_data_by_area("id", rec_area=test_area)
+print(f"\nHouse sensor records for area {test_area} = {len(recs)}")
 # Module 3 code here:
 
 # Module 4 code here:
