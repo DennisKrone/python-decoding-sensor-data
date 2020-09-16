3ad826138f1121df70c4fd4c1dd5f6269b6d3489
Dennis Kroné <dennis.krone@svenskaspel.se>
2020-09-16 09:52:43 +0200

    implement module5

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -4,6 +4,7 @@ from load_data import load_sensor_data
 from temperature_info import TemperatureData
 from humidity_info import HumidityData
 from particle_count_info import ParticleData
+from energy_info import EnergyData
 from datetime import datetime
 from statistics import mean
 
@@ -72,4 +73,19 @@ concentrations = particle_data.get_data_concentrations(data=recs)
 print(f"\tGood Air Quality Recs: {concentrations['good']}")
 print(f"\tModerate Air Quality Recs: {concentrations['moderate']}")
 print(f"\tBad Air Quality Recs: {concentrations['bad']}")
+
 # Module 5 code here:
+energy_data = EnergyData(data)
+recs = energy_data.get_data_by_area(rec_area=test_area)
+print(f"\nHouse Energy sensor records for area {test_area} = {len(recs)}")
+
+total_energy = energy_data.calculate_energy_usage(data=recs)
+print(f"\tEnergy Usage: {total_energy:2.2} Watts")
+
+recs = energy_data.get_data_by_date(rec_date=test_date)
+print(
+    f"House Energy sensor records for date: {test_date.strftime('%m/%d/%y')} = {len(recs)}"
+)
+
+total_energy = energy_data.calculate_energy_usage(data=recs)
+print(f"\tEnergy Usage: {total_energy:2.2} Watts")
